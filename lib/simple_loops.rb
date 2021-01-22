ea0f5ef148bbc9e04642a7876281ba44d267ced6
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-22 20:33:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
 # Write your methods here
+# def method_name(parameter)
+#   count = 0
+#   while count < (something)
+#     # do something with the parameter
+#     count += 1
+#   end
+# end
+def loop_message_five_times (array)
+  count = 0
+  while count <loop_message_five_times
+    do
+    count += 1
+  end
+end
